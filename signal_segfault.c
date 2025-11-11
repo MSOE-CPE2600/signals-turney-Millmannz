3da97063cd23f8be51180c5958f43a22a577da35
Millmannz <millmannz@msoe.edu>
2025-11-10 21:57:08 -0600

    signal_segfault finished

--- signal_segfault.c
+++ signal_segfault.c
@@ -10,11 +10,20 @@
  */
 
 
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
+
+void handle_signal(){
+    printf("Segmentation fault received\n");
+}
 
 int main (int argc, char* argv[]) {
     // Declare a null pointer
     int* i = NULL;
+
+    signal(SIGSEGV, handle_signal);
 
     // Dereference the null pointer
     printf("The value of i is: %d\n", *i);
