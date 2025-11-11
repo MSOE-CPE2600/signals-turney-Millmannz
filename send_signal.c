440500c75cfc172829199f53a0b897ad1c856a48
Millmannz <millmannz@msoe.edu>
2025-11-10 21:55:55 -0600

    send_signal finished

--- send_signal.c
+++ send_signal.c
@@ -1,6 +1,33 @@
 /**
- * File: 
- * Modified by:
+ * File: send_signal.c
+ * Modified by: Zac Millmann
  * 
- * Brief summary of program:
+ * Brief summary of program: Program sends alarm signal and waits until signal is received
  */
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <wait.h>
+
+
+void handle_signal() {
+    printf("Received signal\n");
+    exit(1);
+}
+
+int main() {
+
+    // Register for the signal
+    signal(SIGALRM, handle_signal);
+
+    // Wait until a signal is received
+    alarm(5);
+
+    while(1) {
+        printf("Sleeping\n");
+        sleep(1);
+    }
+
+    return 0;
+}
